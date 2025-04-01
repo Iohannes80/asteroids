f65b3d4a3f39bd3a234210bae1833ad5872c4af3
Iohannes80 <janos.becsi@gmail.com>
2025-04-02 00:53:38 +0200

    Ship is rotating.

--- player.py
+++ player.py
@@ -17,3 +17,18 @@ class Player(CircleShape):
     
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        print("Update being called")  # This should print every frame
+
+        if keys[pygame.K_a]:
+            print("A key pressed")
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            print("D key pressed")
+            self.rotate(dt)
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+        print(f"Rotation: {self.rotation}")
