01ab91d482995d842cb39226a249f34507b27a75
Iohannes80 <janos.becsi@gmail.com>
2025-04-13 12:04:23 +0200

    Shooting draws.

--- player.py
+++ player.py
@@ -1,6 +1,8 @@
 import pygame
 from circleshape import CircleShape
 from constants import *
+from shot import Shot
+
 class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
@@ -35,6 +37,9 @@ class Player(CircleShape):
             print("D key pressed")
             self.move(dt)
 
+        if keys[pygame.K_SPACE]:
+            self.shoot()
+
 
     def rotate(self, dt):
         self.rotation += PLAYER_TURN_SPEED * dt
@@ -43,3 +48,7 @@ class Player(CircleShape):
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
+
+    def shoot(self):
+        shot = Shot(self.position.x, self.position.y)
+        shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
